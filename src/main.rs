9712567bb1ad7be1ccb3457b033bef08ab7bc91a
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-03-23 03:32:58 -0400

    add atomic_symlink

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ fn main() -> std::io::Result<()> {
     atomic_remove_file("/home/pog/text.rsssss",".tmp")?;
     atomic_create_dir("/home/pog/russsstttt",".tmp")?;
     atomic_remove_dir("/home/pog/russsstttt",".tmp")?;
+    atomic_write("/home/pog/text.rsssss1",".tmp", "hi_rust_world")?;
+    atomic_write("/home/pog/text.rsssss2",".tmp", "I_AM_DIFFRENT")?;
+    atomic_remove_file("/home/pog/text.rsssss1",".tmp")?;
+    
+    atomic_symlink("/home/pog/text.rsssss1",".tmp","/home/pog/text.rsssss2" )?;
+
     Ok(())
 }
 
@@ -55,7 +61,6 @@ fn atomic_write<P:AsRef<Path>, E:AsRef<OsStr>, C: AsRef<[u8]> >(path:P, tmp_exte
 //     rename(&tmp_path, &path)?; //If this fails a tmp_file is left in the directory but path isint modified
 //     Ok(())
 // }
-//fs::symlink("a.txt", "b.txt")?;
 
 use std::fs::remove_file;
 
@@ -83,12 +88,18 @@ fn atomic_remove_dir<P:AsRef<Path>, E:AsRef<OsStr>>(path:P, tmp_extension:E) -> 
 
 use std::fs::create_dir;
 
-fn atomic_create_dir<P:AsRef<Path>, E:AsRef<OsStr>>(path:P, tmp_extension:E) -> std::io::Result<()> {
-        atomic_construct!(path,tmp_extension, create_dir())
+fn atomic_create_dir<P:AsRef<Path>, E:AsRef<OsStr>>(path:P, tmp_extension:E) -> std::io::Result<()> {    
+    atomic_construct!(path,tmp_extension, create_dir())
 }
 
+use std::os::unix::fs::symlink;
 
+fn atomic_symlink<P:AsRef<Path>, E:AsRef<OsStr>>(path:P, tmp_extension:E, source:P) -> std::io::Result<()> {
+    let (path,tmp_extension) = (path.as_ref(),tmp_extension.as_ref());
+    let mut tmp_path = PathBuf::from(path);
+    tmp_path.add_extension(tmp_extension);
 
-
-
-
+    symlink(source, &tmp_path)?; //ORDER IS FLIPPED
+    rename(&tmp_path, &path)?; 
+    Ok(())     
+}
