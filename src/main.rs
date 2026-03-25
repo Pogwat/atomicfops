47a13a4da7e6c5459f65bd6948465c07831b1f43
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-03-24 21:33:41 -0400

    add function: add_extension_to_path and use it in macros

--- src/main.rs
+++ src/main.rs
@@ -19,24 +19,18 @@ fn main() -> std::io::Result<()> {
 
 
     macro_rules! atomic_construct {
-    ($pathref:ident, $tmp_extension_ref:ident, $name:ident($($arg:ident),*)) => {{
-        let (path,tmp_extension) = ($pathref.as_ref(),$tmp_extension_ref.as_ref());
-        let mut tmp_path = PathBuf::from(path);
-        tmp_path.add_extension(tmp_extension);
-
+    ($path:expr, $tmp_extension:expr, $name:ident($($arg:ident),*)) => {{
+        let tmp_path = add_extension_to_path($path,$tmp_extension);
         $name(&tmp_path, $($arg),*)?;
-        rename(&tmp_path, &path)?; 
+        rename(&tmp_path, $path)?; 
         Ok(())     
     }};
 }
 
     macro_rules! atomic_destruct {
-    ($pathref:expr, $tmp_extension_ref:expr, $name:ident($($arg:expr),*)) => {{
-        let (path,tmp_extension) = ($pathref.as_ref(),$tmp_extension_ref.as_ref());
-        let mut tmp_path = PathBuf::from(path);
-        tmp_path.add_extension(tmp_extension);
-
-        rename(&path, &tmp_path)?; 
+    ($path:expr, $tmp_extension:expr, $name:ident($($arg:expr),*)) => {{
+        let tmp_path = add_extension_to_path($path,$tmp_extension);
+        rename($path, &tmp_path)?; 
         $name(&tmp_path, $($arg),*)?;
         Ok(())
         
@@ -44,52 +38,35 @@ fn main() -> std::io::Result<()> {
 }
 
 
+fn add_extension_to_path<P:AsRef<Path>,E:AsRef<OsStr>>(path:&P, extension:&E) -> PathBuf {
+    let (path,extension) = (path.as_ref(),extension.as_ref());
+    let mut new_path = PathBuf::from(path);
+    new_path.add_extension(extension);
+    new_path
+}
+
 use std::fs::write;
 
 fn atomic_write<P:AsRef<Path>, E:AsRef<OsStr>, C: AsRef<[u8]> >(path:P, tmp_extension:E, content:C ) -> std::io::Result<()> {
-    atomic_construct!(path,tmp_extension, write(content))
+    atomic_construct!(&path,&tmp_extension, write(content))
 }
-
-//SAME AS BELOW
-
-// fn atomic_write<P:AsRef<Path>, E:AsRef<OsStr>, C: AsRef<[u8]> >(path:P, tmp_extension:E, content:C ) -> std::io::Result<()> {
-//     let (path,tmp_extension) = (path.as_ref(),tmp_extension.as_ref());
-//     let mut tmp_path = PathBuf::from(path);
-//     tmp_path.add_extension(tmp_extension);
-
-//     write(&tmp_path, content)?;
-//     rename(&tmp_path, &path)?; //If this fails a tmp_file is left in the directory but path isint modified
-//     Ok(())
-// }
 
 use std::fs::remove_file;
 
 fn atomic_remove_file<P:AsRef<Path>, E:AsRef<OsStr>>(path:P, tmp_extension:E) -> std::io::Result<()> {
-    atomic_destruct!(path,tmp_extension, remove_file())
+    atomic_destruct!(&path,&tmp_extension, remove_file())
 }
-
-//SAME AS BELOW
-
-// fn atomic_remove_file<P:AsRef<Path>, E:AsRef<OsStr>>(path:P, tmp_extension:E) -> std::io::Result<()> {
-//     let (path,tmp_extension) = (path.as_ref(),tmp_extension.as_ref());
-//     let mut tmp_path = PathBuf::from(path);
-//     tmp_path.add_extension(tmp_extension);
-
-//     rename(&path,&tmp_path)?;
-//     remove_file(&tmp_path)?;
-//     Ok(())
-// }
 
 use std::fs::remove_dir;
 
 fn atomic_remove_dir<P:AsRef<Path>, E:AsRef<OsStr>>(path:P, tmp_extension:E) -> std::io::Result<()> {
-    atomic_destruct!(path,tmp_extension,remove_dir() )
+    atomic_destruct!(&path,&tmp_extension,remove_dir() )
 }
 
 use std::fs::create_dir;
 
 fn atomic_create_dir<P:AsRef<Path>, E:AsRef<OsStr>>(path:P, tmp_extension:E) -> std::io::Result<()> {    
-    atomic_construct!(path,tmp_extension, create_dir())
+    atomic_construct!(&path,&tmp_extension, create_dir())
 }
 
 use std::os::unix::fs::symlink;
